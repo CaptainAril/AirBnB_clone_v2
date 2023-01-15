37a6bc38a19445d1ec957c697d2a9cf421721195
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-15 22:37:00 +0100

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,17 +8,17 @@ from models.state import State
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown_db(exception):
-    """Removes SQLAlchemy session after each request."""
-    storage.close()
-
-
 @app.route('/states_list', strict_slashes=False)
 def states():
     """Displays a HTML page of States."""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Removes SQLAlchemy session after each request."""
+    storage.close()
 
 
 if __name__ == "__main__":
