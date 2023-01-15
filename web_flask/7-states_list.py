285afc68fc3192158ad2219f550c6f274bec6bea
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-15 22:17:44 +0100

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Starts a flask web app."""
 
 from flask import Flask, render_template
 from models import storage
@@ -6,14 +7,19 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown_db(f):
+    """Removes SQLAlchemy session after each request."""
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def states():
+    """Displays a HTML page of States."""
     states = storage.all(State)
     return render_template('7-states_list.html', states=states)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
