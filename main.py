b84c393a4f0ca88c8e4875c8c721ea74bde69c3e
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-15 16:03:39 +0100

    .

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-"""Test cities access from a state"""
-
+"""
+ Test cities access from a state
+"""
 from models import storage
 from models.state import State
 from models.city import City
 
-"""Object creations"""
-
+"""
+ Objects creations
+"""
 state_1 = State(name="California")
 print("New state: {}".format(state_1))
 state_1.save()
@@ -25,10 +27,11 @@ print("New city: {} in the state: {}".format(city_2_1, state_2))
 city_2_1.save()
 
 
-"""Verification"""
+"""
+ Verification
+"""
 print("")
 all_states = storage.all(State)
 for state_id, state in all_states.items():
     for city in state.cities:
         print("Find the city {} in the state {}".format(city, state))
-
