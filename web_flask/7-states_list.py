1629a3e3ab0b66dfd47f2fe959d7b29dd9af919d
CaptainAril <obeemmanuelifeoluwa@gmail.com>
2023-01-16 12:38:44 +0100

    .

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" Starts a flask web app."""
-
+''' connect flask to storage '''
 from flask import Flask, render_template
 from models import storage
+
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays a HTML page of States."""
+    ''' lists state in db storage '''
     states = storage.all('State').values()
     return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def close_db(exception):
-    """Removes SQLAlchemy session after each request."""
+    ''' remove SQLAlchemy Session after each request '''
     storage.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
